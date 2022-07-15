e33b2d1425e9396dce03e91e932b5cb06fa78708
Darksied95 <aliyuefe9501@gmail.com>
2022-07-15 21:48:45 +0100

    final steps added

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,17 +1,38 @@
 import { useState } from "react";
-
 import Board from "./Board";
 import { calculateWinner } from "./../helpers";
 
+const styles = {
+  width: "200px",
+  margin: "20px auto",
+};
 const Game = () => {
-  const [board, setboard] = useState(Array(9).fill(null));
+  const [board, setBoard] = useState(Array(9).fill(null));
   const [xIsNext, seXIsNext] = useState(true);
   const winner = calculateWinner(board);
-  const handleClick = () => {};
+  const handleClick = (i) => {
+    const boardCopy = [...board];
+    if (winner || boardCopy[i]) return;
+    boardCopy[i] = xIsNext ? "X" : "O";
+    setBoard(boardCopy);
+    seXIsNext(!xIsNext);
+  };
   const jumpTo = () => {};
-  const renderMoves = () => {};
+  const renderMoves = () => (
+    <button onClick={() => setBoard(Array(9).fill(null))}>Start Game</button>
+  );
 
-  return <Board squares={board} onClick={handleClick} />;
+  return (
+    <>
+      <Board squares={board} onClick={handleClick} />
+      <div style={styles}>
+        <p>
+          {winner ? `Winner ${winner}` : `Player ${xIsNext ? "X" : "O"} turn`}
+        </p>
+        {renderMoves()}
+      </div>
+    </>
+  );
 };
 
 export default Game;
